7aad551b5ee555320839527a8eb9e1e5aafa5bdb
ilhambr05 <ilhambr05@gmail.com>
2023-08-12 11:22:43 +0700

    section about us

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import HeroBg from '../assets/hero-bg.png'
 import HeroImage from '../assets/hero.svg'
+import AboutUsImage from '../assets/about-us.svg'
 
 function HomePage() {
     const heroBgStyle = {
@@ -30,7 +31,25 @@ function HomePage() {
                     <img className="hero-image" src={HeroImage} alt="Hero Image" />
                 </Container>
             </header>
-            <div id="about" className="min-vh-100 bg-dark">
+            <div id="about" className="min-vh-100 bg-dark text-white d-flex align-items-center px-5">
+                <Container className="container-md">
+                    <Row className="flex-md-row-reverse align-items-center g-5 py-5">
+                        <Col md={6}>
+                            <img className="w-100" src={AboutUsImage} alt="Hero Image" />
+                        </Col>
+                        <Col md={6}>
+                            <h1 className="fw-bold">ABOUT US</h1>
+                            <p>
+                                Officia pariatur non magna incididunt ex ut.
+                                Laborum ad ipsum fugiat sint duis nostrud fugiat.
+                                Enim est qui exercitation reprehenderit amet qui ea.
+                                Minim eu duis esse laborum quis dolor labore.
+                                Mollit nisi esse duis nisi fugiat.
+                                Magna consectetur commodo excepteur reprehenderit sit non sint non sint.
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
             <div id="pricing" className="min-vh-100">
             </div>
