2acfef9daa898e9c192dfd3941ea24d926f89b57
ilhambr05 <ilhambr05@gmail.com>
2023-08-12 07:11:40 +0700

    navbar designs

--- soal-test-2/src/components/NavbarComponents.jsx
+++ soal-test-2/src/components/NavbarComponents.jsx
@@ -1,16 +1,31 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
+import { Button, Container, Nav, Navbar } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+
+import brandLogo from '../assets/brand-logo.png'
 
 function NavbarComponents() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <NavLink to={'/'} className="fs-2 fw-bold navbar-brand">
+                    <img
+                        alt="brand logo"
+                        src={brandLogo}
+                        className="brand-logo d-inline-block align-top"
+                    />{' '}
+                    HOME
+                </NavLink>
+                {/* <Navbar.Brand href="#home">HOME</Navbar.Brand> */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav className="ms-auto text-center">
+                        <Nav.Link href="#about" className="fs-2 fw-bold px-3">ABOUT</Nav.Link>
+                        <Nav.Link href="#pricing" className="fs-2 fw-bold px-3">PRICING</Nav.Link>
+                        <Nav.Link href="#contact" className="fs-2 fw-bold px-3">CONTACT</Nav.Link>
                     </Nav>
+                    <div className="text-center">
+                        <Button variant="outline-info" className="login-btn">Login</Button>
+                    </div>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
