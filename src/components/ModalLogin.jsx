9e46343a0c03eb755a1be713a414956e762e191c
ilhambr05 <ilhambr05@gmail.com>
2023-08-12 13:13:32 +0700

    enhance login

--- src/components/ModalLogin.jsx
+++ src/components/ModalLogin.jsx
@@ -5,37 +5,37 @@ function ModalLogin({ isShown, handleClose }) {
     return (
         <>
             <Modal centered="true" dialogClassName="w-400px" show={isShown} onHide={handleClose}>
-                {/* <Modal.Header>
-                    <Modal.Title>Login</Modal.Title>
-                </Modal.Header> */}
                 <Modal.Body className='p-5'>
-                    <h1 className="h3 mb-5 fw-normal text-center">Login</h1>
-                    <Form>
-                        <Form.Group className="mb-3">
-                            <FloatingLabel
-                                controlId="floatingInput"
-                                label="Email address"
-                                className="mb-3"
-                            >
-                                <Form.Control type="email" placeholder="Email" />
-                            </FloatingLabel>
-                            <FloatingLabel
-                                controlId="floatingPassword"
-                                label="Password"
-                                className='mb-3'
-                            >
-                                <Form.Control type="password" placeholder="Password" />
-                            </FloatingLabel>
-                            <div key="check-remember-login" className="mb-3">
-                                <Form.Check
-                                    type="checkbox"
-                                    id="check-remember-login"
-                                    label="Remember me"
-                                />
-                            </div>
-                        </Form.Group>
-                        <Button variant='primary' size='lg' className='w-100'>Login</Button>
-                    </Form>
+                    <h1 className="h3 mb-5 fw-normal text-center">
+                        Login
+                    </h1>
+                    <Form.Group className="mb-3">
+                        <FloatingLabel
+                            controlId="floatingEmailLogin"
+                            label="Email address"
+                            className="mb-3"
+                        >
+                            <Form.Control type="email" placeholder="Email" />
+                        </FloatingLabel>
+
+                        <FloatingLabel
+                            controlId="floatingPasswordLogin"
+                            label="Password"
+                            className='mb-3'
+                        >
+                            <Form.Control type="password" placeholder="Password" />
+                        </FloatingLabel>
+
+                        <div key="check-remember-login" className="mb-4">
+                            <Form.Check
+                                type="checkbox"
+                                id="check-remember-login"
+                                label="Remember me"
+                            />
+                        </div>
+                    </Form.Group>
+                    <Button variant='outline-info' size='lg' className='w-100'>Login</Button>
+                    <Button variant='link' className='w-100' onClick={handleClose}>Close</Button>
                 </Modal.Body>
             </Modal>
         </>
